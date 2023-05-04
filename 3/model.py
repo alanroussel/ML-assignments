fix batch normalization backpropagation

3/model.py
@@ -2,6 +2,7 @@ import numpy as np
 from etaCycle import EtaCycle
 
 epslion = 1e-6
+alpha = 0.1
 def softmax(x):
 	""" Standard definition of the softmax function """
 	return np.exp(x) / np.sum(np.exp(x), axis=0)
@@ -35,31 +36,55 @@ class Model:
 					"weight":np.random.normal(0,1/np.sqrt(layers_sizes[l]), (layers_sizes[l+1], layers_sizes[l])), 
 					"bias": np.random.normal(0, 0, (layers_sizes[l+1], 1)),
 					"scale":np.random.normal(0,1/np.sqrt(layers_sizes[l+1]), (layers_sizes[l+1],1)),
-					"shift":np.random.normal(0,1/np.sqrt(layers_sizes[l+1]), (layers_sizes[l+1],1))
+					"shift":np.random.normal(0,1/np.sqrt(layers_sizes[l+1]), (layers_sizes[l+1],1)),
+					"mean": np.zeros((layers_sizes[l+1],1)),
+					"variance": np.zeros((layers_sizes[l+1],1))
 			})
 
+		#remove useless info for last layer
 		self.layers[-1].pop("scale")
 		self.layers[-1].pop("shift")
+		self.layers[-1].pop("mean")
+		self.layers[-1].pop("variance")
+
 		self.weight_decay_parameter = weight_decay_parameter
 		self.etaCycle = EtaCycle(1e-5, 1e-1, ns)
 
 		print(f'model \n size of layers : {layers_sizes} \n weight decay parameter : {weight_decay_parameter} \n')
 	
-	def evaluate(self, X):
+	def evaluate(self, X, step):
+		'''
+		X in the input, the flatten image
+		step is training or testing time 
+		During testing, the un-normalized scores are normalized by known pre-computed means and variances that have been estimated during training
+		During training, we computed them directly from the batch
+		'''
 		X = X.T
 		metrics = [(0,0,X,0,0)]
 		for layer in self.layers[0:-1]:
 			S = np.add(np.matmul(layer["weight"], X), layer["bias"])
 			
-			# compute and apply batch normalization
-			mean = np.average(S, 1)
-			var = np.var(S, 1)
-			Sc = np.divide(S-mean.reshape(mean.shape[0], 1), np.sqrt(var.reshape(var.shape[0], 1)) + epslion)
-			Sv = np.add(np.multiply(Sc, layer["scale"]), layer["shift"])
+			if step=="training_time":
+				# compute batch normalization
+				mean, var = np.average(S, 1), np.var(S, 1)
 
-			X = ReLU(Sv)
+				if np.array_equal(layer["mean"], np.zeros(layer["mean"].shape)):
+					layer["mean"] = mean
+					layer["variance"] = var
 
-			metrics.append((S, Sc, X, mean, var))
+				# same mean and variance for test time
+				layer["mean"] = alpha*layer["mean"] + (1-alpha)*mean
+				layer["variance"] = alpha*layer["variance"] + (1-alpha)*var
+
+			elif step=="testing_time":
+				mean = layer["mean"]
+				var = layer["variance"]
+			S_hat = np.divide(S-mean.reshape(mean.shape[0], 1), np.sqrt(var.reshape(var.shape[0], 1)) + epslion)
+			S_wave = np.add(np.multiply(S_hat, layer["scale"]), layer["shift"])
+
+			X = ReLU(S_wave)
+
+			metrics.append((S, S_hat, X, mean, var))
 
 		P = softmax(np.add(np.matmul(self.layers[-1]["weight"], X), self.layers[-1]["bias"]))
 
@@ -89,12 +114,13 @@ class Model:
 			accuracy = np.sum(np.argmax(P, 0) == np.argmax(Y, 0))/batch_size
 		return loss, cost, accuracy
 
-	def computeGradsNum(self, X, Y,P,h=1e-6):
+	def computeGradsNum(self, X, Y,P,h, analytical):
 		""" Converted from matlab code """
 		grad_W1 = np.zeros(self.layers[0]["weight"].shape)
 		grad_b1 = np.zeros(self.layers[0]["bias"].shape)
 
 		X = X.T
+		Pcopy = np.copy(P)
 
 		c, _, _ = self.computeCost(Y, P, andAccuracy=False)
 		
@@ -103,11 +129,21 @@ class Model:
 				W_try = np.array(self.layers[0]["weight"])
 				W_try[i,j] += h
 				Xtry = np.copy(X)
-				Xtry = ReLU(np.add(np.matmul(W_try, Xtry), self.layers[0]["bias"])) 
+
+				Stry = np.add(np.matmul(W_try, Xtry), self.layers[0]["bias"])
+				mean, var = np.average(Stry, 1), np.var(Stry, 1)
+				Stry_hat = np.divide(Stry-mean.reshape(mean.shape[0], 1), np.sqrt(var.reshape(var.shape[0], 1)) + epslion)
+				Stry_wave = np.add(np.multiply(Stry_hat, self.layers[0]["scale"]), self.layers[0]["shift"])
+				Xtry = ReLU(Stry_wave)
+
 				for layer in self.layers[1:-1]:
-					Xtry = ReLU(np.add(np.matmul(layer["weight"], Xtry), layer["bias"])) 
-				P = softmax(np.add(np.matmul(self.layers[-1]["weight"], Xtry), self.layers[-1]["bias"]))
-				c2, _, _ = self.computeCost(Y, P, andAccuracy=False)
+					Stry = np.add(np.matmul(layer["weight"], Xtry), layer["bias"])
+					mean, var = np.average(Stry, 1), np.var(Stry, 1)
+					Stry_hat = np.divide(Stry-mean.reshape(mean.shape[0], 1), np.sqrt(var.reshape(var.shape[0], 1)) + epslion)
+					Stry_wave = np.add(np.multiply(Stry_hat, layer["scale"]), layer["shift"])
+					Xtry = ReLU(Stry_wave)
+				Ptry = softmax(np.add(np.matmul(self.layers[-1]["weight"], Xtry), self.layers[-1]["bias"]))
+				c2, _, _ = self.computeCost(Y, Ptry, andAccuracy=False)
 				grad_W1[i,j] = (c2-c) / h
 		
 		
@@ -115,11 +151,21 @@ class Model:
 			b_try = np.array(self.layers[0]["bias"])
 			b_try[i] += h
 			Xtry = np.copy(X)
-			Xtry = ReLU(np.add(np.matmul(self.layers[0]["weight"],Xtry), b_try)) 
+
+			Stry = np.add(np.matmul(self.layers[0]["weight"], Xtry), b_try)
+			mean, var = np.average(Stry, 1), np.var(Stry, 1)
+			Stry_hat = np.divide(Stry-mean.reshape(mean.shape[0], 1), np.sqrt(var.reshape(var.shape[0], 1)) + epslion)
+			Stry_wave = np.add(np.multiply(Stry_hat, self.layers[0]["scale"]), self.layers[0]["shift"])
+			Xtry = ReLU(Stry_wave)
+
 			for layer in self.layers[1:-1]:
-				Xtry = ReLU(np.add(np.matmul(layer["weight"], Xtry), layer["bias"])) 
-			P = softmax(np.add(np.matmul(self.layers[-1]["weight"], Xtry), self.layers[-1]["bias"]))
-			c2, _, _ = self.computeCost(Y, P, andAccuracy=False)
+				Stry = np.add(np.matmul(layer["weight"], Xtry), layer["bias"])
+				mean, var = np.average(Stry, 1), np.var(Stry, 1)
+				Stry_hat = np.divide(Stry-mean.reshape(mean.shape[0], 1), np.sqrt(var.reshape(var.shape[0], 1)) + epslion)
+				Stry_wave = np.add(np.multiply(Stry_hat, layer["scale"]), layer["shift"])
+				Xtry = ReLU(Stry_wave)
+			Ptry = softmax(np.add(np.matmul(self.layers[-1]["weight"], Xtry), self.layers[-1]["bias"]))
+			c2, _, _ = self.computeCost(Y, Ptry, andAccuracy=False)
 			grad_b1[i] = (c2-c) / h
 		
 		
@@ -129,7 +175,7 @@ class Model:
 	def computeGradsAnalytical(self,X,Y,P, metrics):
 
 		#unzip metrics
-		Ss, Scs, Xs, Ms, Vs = zip(*metrics)
+		Ss, S_hats, Xs, Ms, Vs = zip(*metrics)
 		
 		N = X.shape[0] #batch size
 		G = -(Y-P)
@@ -145,7 +191,7 @@ class Model:
 
 		for layer_id, layer in enumerate(reversed(self.layers[0:-1])):
 			if layer_id != len(self.layers)-1:
-				grad_scale = np.average(np.multiply(G, Scs[-1-layer_id]), 1).reshape(layer["scale"].shape)
+				grad_scale = np.average(np.multiply(G, S_hats[-1-layer_id]), 1).reshape(layer["scale"].shape)
 				grad_shift = np.average(G,1).reshape(layer["shift"].shape)
 
 				# Propagate the gradients through the scale and shift
@@ -159,9 +205,12 @@ class Model:
 				G2 = np.multiply(G, gamma2.reshape(gamma2.shape[0],1))
 				D = np.subtract(Ss[-1-layer_id], Ms[-1-layer_id].reshape(Ms[-1-layer_id].shape[0], 1))
 
-				G = np.subtract(np.subtract(G1, np.average(G1,1).reshape(G1.shape[0],1)), np.multiply(D, np.multiply(G2,D))/N)
+				c = np.sum(np.multiply(G2,D), 1)
+				c = c.reshape(c.shape[0],1)
+				G = np.subtract(np.subtract(G1, np.average(G1,1).reshape(G1.shape[0],1)), np.multiply(D,np.matmul(c,np.ones((1,N))))/N)
+				#G1 - np.matmul(np.sum(G1,1).reshape(12,1),np.ones((1,10)))/N - np.multiply(D,np.matmul(c.reshape(12,1), np.ones((1,10))))/N
 
-				grad_weight = np.matmul(G, Xs[-1-layer_id-1].T) + 2*self.weight_decay_parameter*layer["weight"]
+				grad_weight = np.matmul(G, Xs[-1-layer_id-1].T)/N + 2*self.weight_decay_parameter*layer["weight"]
 				grad_bias = np.average(G,1).reshape(layer["bias"].shape)
 				G = np.matmul(layer["weight"].T, G)
 				G = np.multiply(G, np.where(Xs[-1-layer_id-1]>0,1,0))

3/optimizer.py
@@ -59,11 +59,11 @@ class Optimizer:
 				X_batch = X[batch_id_start:batch_id_end, :]
 				Y_batch = Y[:, batch_id_start:batch_id_end]
 
-				H, Y_pred = self.model.evaluate(X_batch)
+				metrics, Y_pred = self.model.evaluate(X_batch, step="training_time")
 
 				print(f'computing error for a batch ... ')
-				analytical  = self.model.computeGradsAnalytical(X_batch, Y_batch,H, Y_pred)
-				numeric = self.model.computeGradsNum(X_batch, Y_batch,Y_pred, h=1e-5)
+				analytical  = self.model.computeGradsAnalytical(X_batch, Y_batch,Y_pred, metrics)
+				numeric = self.model.computeGradsNum(X_batch, Y_batch,Y_pred, 1e-6, analytical)
 				layers_name_to_print = ["W1", "b1"]
 				for i, name in enumerate(layers_name_to_print):
 					error = np.max(np.abs(analytical[0][i]-numeric[i])) / np.maximum(1e-6,np.abs(np.max(analytical[0][i])) + np.abs(np.max(numeric[i])))
@@ -85,14 +85,14 @@ class Optimizer:
 				X_batch = X[batch_id_start:batch_id_end, :]
 				Y_batch = Y[:, batch_id_start:batch_id_end]
 
-				metrics, Y_pred = self.model.evaluate(X_batch)
+				metrics, Y_pred = self.model.evaluate(X_batch, step="training_time")
 				self.model.backpropagate(X_batch, Y_batch,Y_pred, metrics)
 
 			# at each epoch
 			if(not (mode=="tuning")):
 				# if mode is training or debugging, we want to see the evolution of train and validation cost etc
-				_, Ptraining = self.model.evaluate(X)
-				_, Pvalidation = self.model.evaluate(X_v)
+				_, Ptraining = self.model.evaluate(X, step="testing_time")
+				_, Pvalidation = self.model.evaluate(X_v, step="testing_time")
 				
 				t_loss, t_cost, t_accuracy= self.model.computeCost(Y, Ptraining)
 				v_loss, v_cost, v_accuracy= self.model.computeCost(Y_v, Pvalidation)
@@ -103,7 +103,7 @@ class Optimizer:
 					print(f'\t valid loss/cost/accuracy = {v_loss:.5g} / {v_cost:.5g} / {v_accuracy:.3g}')
 			
 		if(mode=="training" or mode=="debug"):
-			_, Ptest = self.model.evaluate(self.dataset.test[0])
+			_, Ptest = self.model.evaluate(self.dataset.test[0],step="testing_time")
 			t_loss,t_cost, t_accuracy = self.model.computeCost(self.dataset.test[1], Ptest)
 			print(f'\n\t test loss/cost/accuracy = {t_loss:.5g} / {t_cost:.5g} / {t_accuracy:.3g}')
 			if(plot):
@@ -111,7 +111,7 @@ class Optimizer:
 			return t_loss,t_cost, t_accuracy
 		
 		elif(mode=="tuning"):
-			_, Pvalidation = self.model.evaluate(X_v)
+			_, Pvalidation = self.model.evaluate(X_v, step="testing_time")
 			v_loss, v_cost, v_accuracy= self.model.computeCost(Y_v, Pvalidation)
 			print(f'\n\t validation loss/cost/accuracy = {v_loss:.5g} / {v_cost:.5g} / {v_accuracy:.3g}')
 			return v_loss, v_cost, v_accuracy